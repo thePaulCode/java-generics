3e5dd7f1181d42db63125689bb68fba0480b3eb8
Paulo Santos <santospaulor21@gmail.com>
2024-06-18 23:07:31 -0300

    generic interface

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,5 @@ module dev.thepaulcode.generico {
     exports dev.thepaulcode.generico.tipolimitado;
     exports dev.thepaulcode.generico.tipogen;
     opens dev.thepaulcode.generico.tipogen to javafx.fxml;
+    opens dev.thepaulcode.generico.interfacegen to javafx.fxml;
 }
